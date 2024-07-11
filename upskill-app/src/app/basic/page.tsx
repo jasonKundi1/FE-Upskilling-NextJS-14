92862bfa425699c76bd0e9141e3abb91777ed843
Jason Kundi <jason@roughtrade.com>
2024-07-11 16:15:04 +0100

    Basic routing, home page and nav

---
+++ upskill-app/src/app/basic/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Basic = () => {
+  return (
+    <div className="px-[20px] py-[50px]">You have reached the basic route</div>
+  )
+}
+
+export default Basic
---
+++ upskill-app/src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import React from "react"
+import Image from "next/image"
+
+const NavbarLogo = () => {
+  return (
+    <div className="flex items-center space-x-2">
+      <Image
+        width={26}
+        height={26}
+        className="h-8 w-8"
+        src="/assets/troll-face-logo.png"
+        alt="Next.js Logo"
+      />
+      <span className="text-lg font-semibold"> Jasons Next.JS Playground</span>
+    </div>
+  )
+}
+export const Navbar = () => {
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-4">
+          <NavbarLogo />
+        </div>
+        <Link href="/">Home</Link>
+        <Link href="/basic">Basic Route</Link>
+        <Link href="/dynamic">Dynamic Route</Link>
+      </div>
+    </nav>
+  )
+}
